2752da0e38317b9ddb58a4d31a65f34788ef92e9
leeyounghuncom <web3dao@daum.net>
2024-06-29 09:53:27 +0900

    가위,바위,보 동작 테스트

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+import random
+
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def main():
-    return "aaa"
+    rps = ["가위", "바위", "보"]
+
+    if request.method == "POST":
+        guest = request.form.get("guest") #게스트
+        bot = random.choice(rps) #봇
+
+
+    return render_template("main.html", data={"g" :guest, "b":bot})
